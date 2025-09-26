ffdf0fb4601ec77f0bf84f074df31309cb7d91ae
Jonathan Meza <jonathanmeza@ticsocial.com.co>
2025-09-26 04:39:16 -0500

    feat: ACTION only for viewsets

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,14 +1,27 @@
 from rest_framework import serializers
 from .models import Appointment, MedicalNote
+from patients.models import Patient
+from doctors.models import Doctor
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
+  patient = serializers.StringRelatedField(read_only=True)
+  doctor = serializers.StringRelatedField(read_only=True)
+  patient_id = serializers.PrimaryKeyRelatedField(
+    source="patient", queryset=Patient.objects.all(), write_only=True
+  )
+  doctor_id = serializers.PrimaryKeyRelatedField(
+    source="doctor", queryset=Doctor.objects.all(), write_only=True
+  )
+
   class Meta:
     model = Appointment
     fields = [
       "id",
-      "patient",
-      "doctor",
+      "patient_id",  # no edita porque solo se selecciona la relación con el paciente
+      "doctor_id",  # no edita porque solo se selecciona la relación con el doctor
+      "patient",  # solo lectura
+      "doctor",  # solo lectura
       "appointment_date",
       "appointment_time",
       "notes",
--- bookings/viewsets.py
+++ bookings/viewsets.py
@@ -1,8 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Appointment
 from .serializers import AppointmentSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class AppointmentViewSet(ModelViewSet):
   queryset = Appointment.objects.all()
   serializer_class = AppointmentSerializer
+
+  @action(
+    ["GET"], detail=True, url_path="medical-history"
+  )  # Funciona solo con viewsets
+  def get_medical_history(self, request, pk):
+    appointment = self.get_object()
+    serializer = AppointmentSerializer(appointment)
+    return Response(serializer.data, status=status.HTTP_200_OK)
